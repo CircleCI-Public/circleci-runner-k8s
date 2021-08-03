04394b1f65a9fd063c4ba2d1f539a60c5c32228b
Jacob Rahme <jacob.rahme@circleci.com>
2021-08-03 13:20:50 -0400

    use cci api to check job status instead of pod logs

--- scripts/wait_for_task_complete.sh
+++ scripts/wait_for_task_complete.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
-count=0
-while [ $count -ne 5 ]
+while true;
 do
-  echo "$(kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')"
-  count=$(kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' | xargs -n1 kubectl logs -p| grep -c 'Reporting task end')
-  echo "waiting, current count: $count"
-  sleep 5
+  job_status=$(curl --request GET --url "https://circleci.com/api/v2/workflow/$CIRCLE_WORKFLOW_ID/job" --header "Circle-Token: $CCI_API_TOKEN" | jq -c '.items[] | select (.name | contains("runner-job")).status')
+
+  echo "Test job status is $job_status"
+
+  if [[ "$job_status" == '"success"' ]]; then
+    exit 0
+  else
+    sleep 10
+  fi
 done
