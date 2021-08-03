610f712a4250baef839d27fbe9d235facbb637dd
Jacob Rahme <jacob.rahme@circleci.com>
2021-08-03 09:27:55 -0400

    allow launch-agent version pinning

--- scripts/wait_for_task_complete.sh
+++ scripts/wait_for_task_complete.sh
@@ -3,7 +3,8 @@
 count=0
 while [ $count -ne 5 ]
 do
-  count=$(kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' | xargs -n1 kubectl logs | grep -c 'Reporting task end')
+  echo "$(kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')"
+  count=$(kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' | xargs -n1 kubectl logs -p| grep -c 'Reporting task end')
   echo "waiting, current count: $count"
   sleep 5
 done
